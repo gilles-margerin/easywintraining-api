7725968d7cf1aa749a0c3b38aeb3573798f28e30
Gilles Margerin <gillesmargerin@gmail.com>
2021-06-12 19:27:56 +0200

    fixed route error

--- routes/editevent.js
+++ routes/editevent.js
@@ -1,7 +1,7 @@
 module.exports = function (fastify, options, done) {
   fastify.route({
     method: "PUT",
-    url: "/api/events",
+    url: "/api/events/:event",
     handler: async (res, res) => {
       try {
         const user = await fastify.mongoose.User.findById(req.body.userId)
@@ -13,8 +13,9 @@ module.exports = function (fastify, options, done) {
         }
       } catch (err) {
         console.log("Error", err)
-        res.status(500).send("Internal server error")
+        res.status(500).send("Error trying to update document")
       }
     }
   })
+  done()
 }
